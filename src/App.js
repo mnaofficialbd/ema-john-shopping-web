d8727a151f200427eaace3464474a8c0159662b4
mnaofficial <mna.official92@gmail.com>
2022-03-21 22:36:48 +0600

    header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <h2>Ema John Simple Web</h2>
     </div>
   );
