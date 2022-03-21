dcbd6967f2652d11b6a5ce75a206f78ccdac58bd
mnaofficial <mna.official92@gmail.com>
2022-03-21 23:58:25 +0600

    product

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css';
 // import "../../../public/products.json"
 
@@ -13,7 +14,11 @@ const Shop = () => {
     return (
         <div className='shop-container'>
             <div className="products-container">
-                <h3>This is products: {products.length}</h3>
+                {
+                    products.map(product => <Product
+                        key={product.id} product={product}
+                    ></Product>)
+                }
             </div>
             <div className="cart-container">
                 <h3>order summary</h3>
